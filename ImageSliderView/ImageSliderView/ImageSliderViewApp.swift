ec244792dcbe130a60e97d45667d9c7f48c3bb17
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-04 10:49:55 +0700

    Image Slider View

---
+++ ImageSliderView/ImageSliderView/ImageSliderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSliderViewApp.swift
+//  ImageSliderView
+//
+//  Created by Adrian Suryo Abiyoga on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageSliderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
